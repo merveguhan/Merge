40e0656e9fb4e76c06f5117b4e8057153293e26b
merveguhan <merve.guhan@gmail.com>
2025-04-21 22:46:14 +0300

    kameradan canlı görüntü alındı

--- Odev 3.py
+++ Odev 3.py
@@ -1,3 +1,18 @@
 # Odev 3
 
-print("Merhaba GitHub!")
+import cv2
+import numpy as np
+
+kamera=cv2.VideoCapture(0)
+
+while True:
+    ret,goruntu=kamera.read()
+
+    cv2.imshow("kameradan", goruntu)
+
+    if cv2.waitKey(30) & 0xFF == ('q'):
+        break
+
+kamera.release()
+
+cv2.destroyAllWindows()
